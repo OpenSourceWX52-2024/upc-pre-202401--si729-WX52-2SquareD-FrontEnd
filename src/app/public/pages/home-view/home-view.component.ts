352aa062fd3d347050149b6292ff00bf9e5de264
Miguel Angel Ybañez Esquerre <u20201b298@upc.edu.pe>
2024-05-03 03:00:24 -0500

    feat: carrousel videogames

--- src/app/public/pages/home-view/home-view.component.ts
+++ src/app/public/pages/home-view/home-view.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-home-view',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './home-view.component.html',
   styleUrl: './home-view.component.css'
 })
 export class HomeViewComponent {
+  items = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5']; // Ejemplo de elementos
+  currentIndex = 0;
 
+  prev() {
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+    }
+  }
+
+  next() {
+    if (this.currentIndex < this.items.length - 3) {
+      this.currentIndex++;
+    }
+  }
 }
