1f45fc5d04011602e4723f8c98eec7084ec70f48
MiguelC <sackito992@gmail.com>
2024-05-04 00:20:17 -0500

    feat: add some components in home view

--- src/app/public/pages/home-view/home-view.component.ts
+++ src/app/public/pages/home-view/home-view.component.ts
@@ -1,16 +1,22 @@
 import { Component } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
-
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home-view',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MatCardModule, CommonModule],
   templateUrl: './home-view.component.html',
   styleUrl: './home-view.component.css'
 })
 export class HomeViewComponent {
-  items = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5']; // Ejemplo de elementos
+  items = [
+    { src: 'assets/game-screen1.png' },
+    { src: 'assets/game-screen1.png' },
+    { src: 'assets/game-screen1.png' },
+    { src: 'assets/game-screen1.png' },
+    { src: 'assets/game-screen1.png' },
+  ];
   currentIndex = 0;
 
   prev() {
