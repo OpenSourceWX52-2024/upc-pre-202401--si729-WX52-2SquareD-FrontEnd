6380f128b0b2c98ac37a0d688505ed976506e1b3
Sharon Antuanet Ivet Barrial Marin <u202114900@upc.edu.pe>
2024-05-04 00:23:12 -0500

    feat: add new components (homeview)
    add button functional: redirect to plans

--- src/app/public/pages/home-view/home-view.component.ts
+++ src/app/public/pages/home-view/home-view.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from "@angular/material/button";
+import { Router } from '@angular/router';
 
 
 @Component({
   selector: 'app-home-view',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './home-view.component.html',
   styleUrl: './home-view.component.css'
 })
@@ -13,6 +15,8 @@ export class HomeViewComponent {
   items = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5']; // Ejemplo de elementos
   currentIndex = 0;
 
+  constructor(private router: Router) {
+  }
   prev() {
     if (this.currentIndex > 0) {
       this.currentIndex--;
@@ -24,6 +28,8 @@ export class HomeViewComponent {
       this.currentIndex++;
     }
   }
-
+  redirectToPremium() {
+    this.router.navigate(['/plans']);
+  }
 
 }
