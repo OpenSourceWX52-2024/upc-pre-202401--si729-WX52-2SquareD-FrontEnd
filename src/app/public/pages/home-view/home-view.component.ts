8813380b091edae7c9fa580c7340df6528df4563
Sharon Antuanet Ivet Barrial Marin <u202114900@upc.edu.pe>
2024-05-16 00:14:10 -0500

    feat: add components for carousel

--- src/app/public/pages/home-view/home-view.component.ts
+++ src/app/public/pages/home-view/home-view.component.ts
@@ -1,10 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import { CommonModule } from '@angular/common';
 import {MatButtonModule} from "@angular/material/button";
 import {Router, RouterLink} from '@angular/router';
-
-
 
 @Component({
   selector: 'app-home-view',
@@ -13,34 +11,113 @@ import {Router, RouterLink} from '@angular/router';
   templateUrl: './home-view.component.html',
   styleUrl: './home-view.component.css'
 })
-export class HomeViewComponent {
-  items = [
-    { src: 'assets/game-screen1.png' },
-    { src: 'assets/game-screen1.png' },
-    { src: 'assets/game-screen1.png' },
-    { src: 'assets/game-screen1.png' },
-    { src: 'assets/game-screen1.png' },
-  ];
+
+export class HomeViewComponent implements OnInit {
   currentIndex = 0;
 
   constructor(private router: Router) {
   }
-  prev() {
-    if (this.currentIndex > 0) {
-      this.currentIndex--;
-    }
+
+  ngOnInit() {
+    this.initializeCarousel();
   }
 
-  next() {
-    if (this.currentIndex < this.items.length - 3) {
-      this.currentIndex++;
+  initializeCarousel() {
+    const CARDS = [
+      "https://www.pokemon-card.com/assets/images/card_images/large/SV2a/043953_P_HITOKAGE.jpg",
+      "https://www.pokemon-card.com/assets/images/card_images/large/SV4a/044997_P_RIZADO.jpg",
+      "https://www.pokemon-card.com/assets/images/card_images/large/S12a/042274_P_RIZADONV.jpg",
+      "https://www.pokemon-card.com/assets/images/card_images/large/SV2a/043491_P_ZENIGAME.jpg",
+      "https://www.pokemon-card.com/assets/images/card_images/large/SV2a/043492_P_KAMERU.jpg",
+      "https://www.pokemon-card.com/assets/images/card_images/large/SV2a/043987_P_KAMEKKUSUEX.jpg",
+      "https://www.pokemon-card.com/assets/images/card_images/large/SV2a/043951_P_FUSHIGIDANE.jpg",
+      "https://www.pokemon-card.com/assets/images/card_images/large/SV2a/043952_P_FUSHIGISOU.jpg",
+      "https://www.pokemon-card.com/assets/images/card_images/large/SV2a/043985_P_FUSHIGIBANAEX.jpg",
+    ];
+
+    const CARD_CONTAINER = document.querySelector<HTMLUListElement>("#cards");
+
+    if (!CARD_CONTAINER) return;
+
+    CARD_CONTAINER.addEventListener('scroll', () => {
+      this.currentIndex = Math.round(CARD_CONTAINER.scrollLeft / CARD_CONTAINER.clientWidth);
+    });
+
+    const PADDING = 4;
+
+    // Add prepadding
+    for (let pre = 0; pre < PADDING; pre++) {
+      const COVER_INDEX = CARDS.length - (PADDING - 1) + pre;
+      const ITEM = Object.assign(document.createElement("li"), {
+        ariaHidden: true,
+        innerHTML: `
+          <div class="image-wrapper">
+            <img src="${CARDS[COVER_INDEX - 1]}" alt="" />
+          </div>
+        `,
+      });
+      CARD_CONTAINER.appendChild(ITEM);
     }
+
+    CARDS.map((card) => {
+      const ITEM = Object.assign(document.createElement("li"), {
+        innerHTML: `
+          <div class="image-wrapper">
+            <img src="${card}" alt="" />
+          </div>
+        `,
+      });
+      CARD_CONTAINER.appendChild(ITEM);
+    });
+
+    // Add post padding
+    for (let post = 0; post < PADDING; post++) {
+      const COVER_INDEX = post;
+      const ITEM = Object.assign(document.createElement("li"), {
+        ariaHidden: true,
+        innerHTML: `
+          <div class="image-wrapper">
+            <img src="${CARDS[COVER_INDEX]}" alt="" />
+          </div>
+        `,
+      });
+      CARD_CONTAINER.appendChild(ITEM);
+    }
+
+    const ITEMS = CARD_CONTAINER && CARD_CONTAINER.children ? Array.from(CARD_CONTAINER.children) : [];
+
+    ITEMS.forEach((ITEM, index) => {
+      if (index <= 2 || index >= ITEMS.length - 4)
+        ITEM.setAttribute("aria-hidden", "true");
+    });
+
+    let scrollBounds = { max: 0, min: 0 };
+
+    const UPDATE = () => {
+      if (CARD_CONTAINER.scrollLeft < scrollBounds.min) {
+        CARD_CONTAINER.scrollLeft = scrollBounds.max;
+      } else if (CARD_CONTAINER.scrollLeft > scrollBounds.max) {
+        CARD_CONTAINER.scrollLeft = scrollBounds.min;
+      }
+    };
+
+    const SET_SCROLL_BOUNDS = () => {
+      (ITEMS[ITEMS.length - 1] as HTMLElement).scrollIntoView();
+      scrollBounds.max = CARD_CONTAINER.scrollLeft + (ITEMS[0] as HTMLElement).offsetWidth;
+      (ITEMS[0] as HTMLElement).scrollIntoView();
+      scrollBounds.min = CARD_CONTAINER.scrollLeft - (ITEMS[0] as HTMLElement).offsetWidth;
+    };
+
+    SET_SCROLL_BOUNDS();
+
+    CARD_CONTAINER.addEventListener("scroll", UPDATE);
   }
+
   redirectToPremium() {
     this.router.navigate(['/plans/:id']);
   }
+
   redirectToProgress() {
     this.router.navigate(['games/:id']);
   }
-
 }
