8301d1e1c8cb52e5a67e238e4aa3195805d0a333
Sharon Antuanet Ivet Barrial Marin <u202114900@upc.edu.pe>
2024-06-24 04:58:24 -0500

    fix: add views to siide bar

--- src/app/public/components/the-sidebar-content/the-sidebar-content.component.ts
+++ src/app/public/components/the-sidebar-content/the-sidebar-content.component.ts
@@ -1,12 +1,12 @@
 import {MediaMatcher} from '@angular/cdk/layout';
-import {ChangeDetectorRef, Component, Input, OnDestroy, OnInit} from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import {MatListModule} from '@angular/material/list';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 
-import {RouterLink, RouterOutlet, ActivatedRoute, Router} from "@angular/router";
+import {RouterLink, RouterOutlet, ActivatedRoute } from "@angular/router";
 
 import {TheFooterContentComponent} from "../the-footer-content/the-footer-content.component";
 
@@ -26,24 +26,32 @@ export class TheSidebarContentComponent implements OnInit{
   userId:any="1";
 
 
-  fillerNav = [{name:"Inicio",path:"/home"}, {name:"Juegos",path:"/games"}, {name:"Progreso",path:"/my-progress"},{name:"Planes", path:"/plans"},{name:"Configuración",path:"/configuration"}];
+readonly fillerNav = [
+  {name:"Inicio",path:"/home"},
+  {name:"Juegos",path:"/games"},
+  {name:"Progreso",path:"/my-progress"},
+  {name:"Planes", path:"/plans"},
+  {name:"Clases", path:"/my-clases"},
+  {name:"Configuración",path:"/configuration"}
+];
 
-
-  private _mobileQueryListener: () => void;
+  private readonly _mobileQueryListener: () => void;
   constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher, private activatedRoute : ActivatedRoute) {
 
     console.log(this.userId);
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
-    this.mobileQuery.addListener(this._mobileQueryListener);
+    this.mobileQuery.addEventListener('change', this._mobileQueryListener);
   }
 
-  ngOnDestroy(): void {
-    this.mobileQuery.removeListener(this._mobileQueryListener);
-  }
+  /**ngOnDestroy(): void {
+   this.mobileQuery.removeListener(this._mobileQueryListener);
+   }*/
 
   ngOnInit() {
-
+  this.activatedRoute.params.subscribe(params => {
+    this.userId = params['id']; // assuming 'id' is a parameter in your route
+  });
   }
 
 }
